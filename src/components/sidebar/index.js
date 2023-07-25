84952abb7bd883617e4083d7eb1d2a98dae28248
Shivaji P Vichare <shivaji.vichare@gmail.com>
2023-07-25 14:10:35 -0400

    Update domain name.

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -84,8 +84,8 @@ export default function Sidebar({ProjectTaskList, setSelectedTask, selectedProje
           <S.ProfileInfo onClick={() => {console.log("Top left image clicked.");
               // Reset the selected task here.
               if(typeof setSelectedTask !== "undefined") setSelectedTask({name:'', id:0});}}>
-            <h2 className="text-base">Onboard.how</h2>
-            <S.Email className="text-sm">support@onboard.how</S.Email>
+            <h2 className="text-base">Onboard.icu</h2>
+            <S.Email className="text-sm">admin@onboard.icu</S.Email>
           </S.ProfileInfo>
         </S.Profile>
         <S.List>
